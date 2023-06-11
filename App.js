584bc584ff8533dad82dd17613330dd65743644b
iamrashidpathan <khanrshid11a@gmail.com>
2023-06-11 13:35:30 +0530

    add episode 3

--- App.js
+++ App.js
@@ -1,5 +1,29 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
-const heading = React.createElement("h1",{id:"heading",attributes:"attributes"},"Hello World From React")
+
+const elem = <span>React Element</span>
+
+// REACT Elememt
+const Title =()=> <h1 className="head"> {elem} Namaste React from JSX 🚀</h1>
+
+// REACT Functional Component
+
+const HeadingComponent = ()=>{
+    return (
+        // <React.Fragment></React.Fragment>
+        <>
+            <div id="container">
+                <Title/>
+                <h1>Namaste React Functional Component</h1>
+            </div>
+            <div id="container2">
+                Container 2
+            </div>
+        </>
+    
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+
+root.render(<HeadingComponent/>) 
